e830efc4439f809d8b5edd935d6c16b95a8beaf8
huandv <dohuan2k1@gmail.com>
2021-09-19 08:43:11 +0700

    abc

---
+++ src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @PostMapping
+    public Student create(@RequestBody Student student){
+       return studentRepository.save(student);
+    }
+
+}
